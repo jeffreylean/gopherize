9c849a88f6f226dc940272288599128365bf6bd4
JeffreyLean <leanwf1117@gmail.com>
2023-03-21 00:31:16 +0800

    Force user to run gopherize command in gopherize directory

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 )
 
 func main() {
+	// Check if user is in the gopherize directory
+	if _, err := os.Stat("exercise.yaml"); err != nil {
+		fmt.Print("gopherize must be run from the gopherize directory!\nTry `cd gopherize/`.")
+		os.Exit(1)
+	}
+
 	rootCmd := cmd.Root()
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprint(os.Stderr, err)
